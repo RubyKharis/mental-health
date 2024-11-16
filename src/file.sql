2e19e758b40d1a3d3d5c5ff669ea53bb6cfaef27
RubyKharis <116410455+RubyKharis@users.noreply.github.com>
2024-11-16 01:04:25 -0700

    WIP

--- src/file.sql
+++ src/file.sql
@@ -1,5 +1,10 @@
 -- Start coding here...
-SELECT stay, COUNT(inter_dom) AS count_int, ROUND(AVG(todep),2) AS average_phq, ROUND(AVG(tosc),2) AS average_scs, ROUND(AVG (toas),2) AS average_as
+SELECT 
+    stay, 
+    COUNT(inter_dom) AS count_int,
+    ROUND(AVG(todep),2) AS average_phq, 
+    ROUND(AVG(tosc),2) AS average_scs, 
+    ROUND(AVG (toas),2) AS average_as
 FROM students
 WHERE inter_dom ='Inter'
 GROUP BY stay
